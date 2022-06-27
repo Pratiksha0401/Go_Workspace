5bc2b2e97633d185b95fa835e1d1481f8585f1fd
Pratiksha0401 <pratiksha411998@gmail.com>
2022-06-27 20:36:56 +0530

    Added : channel in go and Updated http

--- core_golang/HTTP/main.go
+++ core_golang/HTTP/main.go
@@ -2,16 +2,37 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 )
 
+type logWriter struct{}
+
 func main() {
 	resp, err := http.Get("http://google.com")
-	if err != nil{
+	if err != nil {
 		fmt.Println("Error")
 		os.Exit(1)
 	}
 
-	fmt.Println(resp)
+	//fmt.Println(resp)
+	//to read body by byte size
+	/*s := make([]byte , 99999)
+	resp.Body.Read(bs)
+	fmt.Println(string(bs))
+	*/
+	//use of writer and reader to get body
+	//io.Copy(os.Stdout , resp.Body)
+
+	//custom writer
+	lw := logWriter{}
+	io.Copy(lw, resp.Body)
+
+}
+
+func (logWriter) Write(bs []byte) (int, error) {
+	fmt.Println(string(bs))
+	fmt.Println("Just wrote this many bytes:", len(bs))
+	return len(bs), nil
 }
